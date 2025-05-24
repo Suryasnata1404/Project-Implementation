a82d05d2ebca9831e38df21389ef2e1b42384304
Suryasnata Mohapatra <suryasnata2004@gmail.com>
2025-05-25 00:04:23 +0530

    Add files via upload

---
+++ conversion-operation.js
@@ -0,0 +1,34 @@
+let isLoggedIn = 1
+let num = Boolean(isLoggedIn)
+//console.log(num);
+
+// 1 => true
+// 11 => true
+// 0 => false
+// "Dudu" => true
+//  "" => false
+// null => false
+// undefined => false
+
+let value = 10 
+let negValue = -value
+//console.log(negValue);
+
+let str1="hello"
+let str2=" sunshine"
+let str3= str1  + str2
+//console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(true)
+console.log(+true) //1
+//console.log(true+) =>error
+
+console.log(+""); //0
+
+
+
