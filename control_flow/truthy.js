356cc758813cebd722faf8d64953da65213a1a92
Suryasnata Mohapatra <suryasnata2004@gmail.com>
2025-05-28 00:01:45 +0530

    Update truthy.js

--- control_flow/truthy.js
+++ control_flow/truthy.js
@@ -30,13 +30,10 @@ let val1;
 // val1 = null ?? 10
 // val1 = undefined ?? 15
 val1 = null ?? 10 ?? 20
-
-
-
+    
 console.log(val1);
 
 // Terniary Operator
-
 // condition ? true : false
 
 const iceTeaPrice = 100
